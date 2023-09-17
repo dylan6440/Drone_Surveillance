9a3e0bb03c147893a12fda8c3286b13918f4ab4b
dylan6440 <70941138+dylan6440@users.noreply.github.com>
2023-09-17 17:25:15 +0200

    Update reco.sh

--- Module_IA/reco.sh
+++ Module_IA/reco.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-edgtpu_detect --source /dev/video1:YUY2:800x600:24/1 --model yolov5/runs/train/exp/weights/bets-fp16.tflite
+edgetpu_detect --source /dev/video1:YUY2:800x600:24/1 --model yolov5/runs/train/exp/weights/bets-fp16.tflite
